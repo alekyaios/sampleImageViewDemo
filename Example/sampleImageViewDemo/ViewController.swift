267f2c9bb1c1663cfcca02f7dcbb342dea307a05
alekyaios <alekhya@krify.com>
2023-02-02 10:29:14 +0530

    changes done

--- Example/sampleImageViewDemo/ViewController.swift
+++ Example/sampleImageViewDemo/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
         var UI = customView()
         UI = (Bundle.main.loadNibNamed("customView", owner: self, options: nil)!.last as? customView)!
         UI.frame = CGRect(x:30, y:30, width: 250 , height:250)
-        self.sampleView.addSubview(UI)
-        UI.sampleImageView.setUIComp(cornerRadius: 50, image: UIImage(named: "bird")!, borderwidth: 1, borderColor: UIColor.black.cgColor, shadowOffset: CGSize(width: 4, height: 5), shadowOpacity: 5)
+        self.view.addSubview(UI)
+        UI.sampleImageView.setUIComp(cornerRadius: 30, image: UIImage(named: "bird")!, borderwidth: 2, borderColor: UIColor.red.cgColor, shadowOffset: CGSize(width: 4, height: 5), shadowOpacity: 20)
     }
     
     override func didReceiveMemoryWarning() {
